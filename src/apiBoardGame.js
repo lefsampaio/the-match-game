0ad90d70aa283b45dba0f9aa5eb239220bb29753
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-11-19 13:31:48 -0300

    alterações no profile e inclusão de API

--- src/apiBoardGame.js
+++ src/apiBoardGame.js
@@ -6,31 +6,16 @@ const getNameGame = () => {
     .then(data => {
       const nameGames = data.games.map(game=>
         game.name);
-    console.log(nameGames);
-     
-    return nameGames
+      const linkGames = data.games.map(game=>
+        game.url);  
+      console.log(nameGames, linkGames);
+      return nameGames, linkGames
       
-    });
-    
-};
+    })
 
-const getLinkGame = () => {
-    fetch(
-      "https://www.boardgameatlas.com/api/search?order_by=popularity&ascending=false&client_id=SB1VGnDv7M "
-    )
-      .then(response => response.json())
-      .then(data => {
-        const linkGames = data.games.map(game=>
-          game.url);
-      console.log(linkGames);
-       
-      return linkGames
-        
-      });
-      
-  };
-
-getNameGame();
-getLinkGame();
+}
+getNameGame()
+ 
+export default getNameGame 
 
 // cardsGiphy.innerHTML = `<img class='gif-card'src="${giphyCards}">`;
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,6 +1,7 @@
 import Button from '../components/button.js'; 
 import Input from '../components/input.js';
 import textArea from '../components/text-area.js';
+import getNameGame from '../apiBoardGame.js'; 
 
 const location = () => {
   location.hash = '';
@@ -11,42 +12,60 @@ const data = avatar
 function buildHtml(data) {
   let html = "";
   data.forEach((obj) => html += `
-  <img class="logo-avatar" src="${obj.url}" id="${obj.id}">`)
+  <img class="logo-avatar" src="${obj.url}" id="${obj.id}" ${Button(chooseAvatar)}>`)
   return html
 }
-        
+
 
 const Profile = () => {
   document.querySelector('body').className = '';
   const template = `
-  <nav class="nav-bar"> The Match Game </nav>
+  <nav class="nav-bar"> 
+   <strong>The Match Game
+   ${Button({
+    type: "button",
+    title: "Voltar",
+    class: "primary-button",
+    onClick: location,
+    disabled: "enabled"
+  })}
+  </nav>
   <section class='container main-container'>
     <section class="container">
     <h1 class="titulo-avatar">ESCOLHA SEU AVATAR</h1>
-    <section>
+    <section class="escolha-avatar">
      ${buildHtml(data)}
     </section>
     </section>
     <section class="container">
       <form class="container" id="register-form">      
-        ${Input({ type: 'text',
-                  placeholder: 'Nome', 
-                  class: 'js-name-input primary-input'
-                })}        
+        ${Input({
+          type: "text",
+          placeholder: "Nome",
+          class: "js-name-input primary-input"
+        })}
+        ${Input({
+          type: "text",
+          placeholder: "Escolha seus 5 jogos favoritos",
+          class: "js-game-name primary-input"
+        })}
+        ${Button({
+          type: "submit",
+          title: "+",
+          class: "btn-add",
+          onClick: chooseGame
+        })}
         ${textArea({
-                  class: 'add-comment',
-                  placeholder: 'Comente...',
-                })}
-        ${Button({type: 'submit',
-                  title: 'Cadastrar',
-                  class: 'primary-button',
-                  onClick: '',
-                })}
-        ${Button({type: 'button', 
-                  title: 'Voltar',
-                  class: 'primary-button',
-                  onClick: location, disabled: 'enabled',
-                })}
+          class: "add-comment list-games",
+          placeholder: "Cinco jogos favoritos..."
+        })}
+        ${Button({
+          type: "submit",
+          title: "Cadastrar",
+          class: "primary-button",
+          onClick: "newUser"
+        })}
+
       </form>
     </section>
   </section>
@@ -55,26 +74,40 @@ const Profile = () => {
 };
 
 
-// cardsGiphy.innerHTML = `<img class='gif-card'src="${giphyCards}">`;
+// nameGames.forEach(item => {
+//   addOption(item)
+// })
 
-// function newUser() {
-//   const userName = document.querySelector('.js-name-input')
-//   //const userImg = document.querySelector('.logo-avatar')
-//   // const userGames = document.querySelector('')
-//   const userProfile = { 
-//     userName: userName.value, 
-//     userImg: id.value,
-//     userGames: [], 
-
-//   };
-//   firebase.firestore().collection('user-profile').add(userProfile);
-
-// }
+function chooseGame() {
+ 
+    const test = document.querySelector('.js-game-name');
+    const option = document.createElement('p');
+    const area = document.querySelector('.add-comment');
+    option.text = "";
+    area.add(test);
+};
 
 
+function chooseAvatar() {
+//   const avatar = document.querySelector('.js-game-name').value; 
+  
+}; 
+
+chooseAvatar()
+
+function newUser() {
+  const userName = document.querySelector('.js-name-input')
+  const userImg = document.querySelector('.logo-avatar')
+  const userGames = document.querySelector('.list-games')
+  firebase.firestore().collection('user-profile').add(userProfile)
+  .then(() => {
+  userName.value = '';
+  userImg.value = '';
+  userGames.value = '';
+})
+}
 
 
 
 
-
-export default Profile;
+export default Profile
